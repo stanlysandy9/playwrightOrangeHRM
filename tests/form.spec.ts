f17f0f4074788871acc262679dfec0ddc4187157
stanlysandy9 <stanlysandy9@gmail.com>
2026-05-29 18:30:04 +0530

    New commit

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -156,4 +156,5 @@
     await expect(cnfmPswrdFld).not.toHaveValue('Monou@1998');
     await expect(tnCFld).not.toBeChecked();
         //Test case completed
+        console.log("test passed")
     })
